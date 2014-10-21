08212b5c7dd5a859f439da8dc466c84c4e8a4afc
Joris Gillis <joris.gillis@mobilevikings.com>
2014-10-21 16:35:41 +0200

    Drawing polygons
    A hand made feature was drawn
    Now some problems with transferring the GeoJSON to the browser in the
    correct format (quotes are escaped).

--- server.R
+++ server.R
@@ -5,5 +5,64 @@ library(reshape2)
 library(jsonlite)
 
 shinyServer(function(input, output) {
-   
+  
+  loadGeoJSON <- reactive({
+    # Loading the administratie boundaries of Belgium
+    geoJSON <- readLines('data/BEL_adm4.geojson')
+    geo.df  <- fromJSON(geoJSON)
+    geo.df  <- head(geo.df) # For testing purposes
+    geo.df$type <- unbox(geo.df$type)
+    geo.df
+  })
+  
+  feature <- reactive({
+    feature <- 
+      list(
+        type = unbox("FeatureCollection"),
+        features = 
+          list(
+            list(
+              type = unbox("Feature"),
+              id   = unbox("feature1"),
+              properties = list(name = "A Rectangle"),
+              geometry = list(
+                type = unbox("LineString"),
+                coordinates = list(list(list(unbox(49.5), unbox(5.1)), 
+                                        list(unbox(51.5), unbox(5.1)), 
+                                        list(unbox(51.5), unbox(4.1)), 
+                                        list(unbox(49.5), unbox(4.1))))
+              )
+            )
+          )
+      )
+    feature
+  })
+  
+  output$map <- renderMap({
+    # Loading GeoJSON data
+    geo.df <- loadGeoJSON()
+    
+    # Creating the map
+    map <- Leaflet$new()
+    map$setView(c(50.5, 4.6), zoom = 8)
+    map$tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    
+    # Adding GeoJSON to the map
+    map$geoJson(toJSON(geo.df), 
+                style = "#! 
+                    function(feature) {
+                      return {
+                        'color': '#cc3333',
+                        'fillColor': '#cc3333',
+                        'weight': 5
+                      }; 
+                    } !#", 
+                onEachFeature = "#!
+                      function(feature, layer) {
+                        console.log(JSON.stringify(feature));
+                        L.polygon(feature.geometry.coordinates).addTo(map);
+                      }
+                    !#")
+    map
+  })
 })
